aaa9ff3c78313f22793c5a27d4e41cf94cfeb968
Mamadou-Ndiaye <ndiamamadou@gmail.com>
2021-01-20 22:15:58 +0000

    petit changement fichier de  pour signaler le demarrage

--- src/main/java/ucad/com/ecommerceSpringBoot/web/DatabaseConfig.java
+++ src/main/java/ucad/com/ecommerceSpringBoot/web/DatabaseConfig.java
@@ -12,7 +12,7 @@ import javax.sql.DataSource;
 @Configuration
 public class DatabaseConfig {
   // Pour dreployer sur heroku cela veut dire qu on a pas besoin ca a part ca
-    System.out.print("Bonjour oiuytrs")
+
     @Value("${postgres://qkzwphjxunpmep:7505750a7b92e945e9b6c212f58767aa1d7b71d1159190ebf41845a66d362d60@ec2-54-75-248-49.eu-west-1.compute.amazonaws.com:5432/d4ndcpa8gm9s41}")
     private String dbUrl;
 
@@ -20,6 +20,7 @@ public class DatabaseConfig {
     public DataSource dataSource() {
         HikariConfig config = new HikariConfig();
         config.setJdbcUrl(dbUrl);
+        System.out.println("Bonjour oiuytrs");
         return new HikariDataSource(config);
     }
 }
